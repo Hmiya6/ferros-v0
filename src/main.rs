d5b2b7d818cec0fbf6c14a641f0149cda1e9581e
hmiya <h.miya.work@gmail.com>
2021-06-24 11:32:01 +0900

    Add test framework

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 
 #![no_std] // don't link the Rust standard library
 #![no_main] // disbale all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 mod vga_buffer;
 
 
@@ -13,13 +16,34 @@ fn panic(_info: &PanicInfo) -> ! { // function that never returns: `-> !` (diver
     loop {}
 }
 
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a funtion
     // named `_start` by default
     
-    
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial asserttion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+
 
