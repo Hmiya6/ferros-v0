f0eb9397e5ea9fa9174589187c7fe06fd52adb6c
hmiya <h.miya.work@gmail.com>
2021-08-30 16:15:38 +0900

    Paging implementation

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@
 #![test_runner(ferros::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use ferros::println;
+use ferros::{println};
 
 // パニック時に呼ばれる関数
 #[cfg(not(test))] // テスト時にはコンパイルしない. 
@@ -22,19 +23,41 @@ fn panic(info: &PanicInfo) -> ! {
     ferros::test_panic_handler(info)
 }
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a funtion
-    // named `_start` by default
-    
+entry_point!(kernel_main);
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use ferros::memory::{self, BootInfoFrameAllocator};
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     println!("Hello World{}", "!");
-    
-    // setup IDT
-    ferros::init();
-    
-    use x86_64::registers::control::Cr3;
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    ferros::init(); // setup IDT
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut page_table = unsafe { memory::init(phys_mem_offset) };
+
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+    let page = Page::containing_address(VirtAddr::new(0)); // address `0` is unused. 
+    memory::create_example_mapping(page, &mut page_table, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // don't write to the start of the page because the top line of the VGA buffer is directly shifted off the screen by hte next `println`
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)} 
+
+    // let addresses = [
+    //     0xb8000, // VGA buffer page
+    //     0x201008, // some code page
+    //     0x0100_0020_1a10, // some stack page
+    //     boot_info.physical_memory_offset, // virtual address mapped to physicall address 0
+    // ];
+
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     let phys = page_table.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
+
+
 
     #[cfg(test)]
     test_main();
@@ -42,7 +65,6 @@ pub extern "C" fn _start() -> ! {
     println!("It did not crash!");
     ferros::hlt_loop(); 
 }
-
 
 #[test_case]
 fn trivial_assertion() {
